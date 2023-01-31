286b9e3ccded6011740ee255c70e015c680a71fc
Muslim1427 <105022109+Muslim1427@users.noreply.github.com>
2023-01-31 15:45:59 +0300

    add new file

---
+++ script.js
@@ -0,0 +1,48 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlides(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i = 0;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += "active";
+}
+
+const btn = document.querySelector(".checkbox");
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+  let theme = "light";
+  if (document.body.classList.conteiners("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
